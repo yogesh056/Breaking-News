5049d249cc96a9cf89a48c93073263cf8cc2ce69
Yogeshwaran <yogeshwaran056@gmail.com>
2019-08-13 10:27:55 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,106 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<div className="header">
+<div id="a">
+<div id="b">
+<p id="d">
+Breaking News
+</p>
+</div>
+<div id="c">
+<p id="e">
+</p>
+</div>
+
+</div>
+    <div id="mainDiv">
+    <div id="boxDiv">
+        <div id="front"> 
+        <p className="p">INDIA TODAY</p>
+        </div>
+        <div id="back">
+        <p className="p">INDIA TODAY</p>
+        </div>
+        <div id="left">
+        <p className="p">INDIA TODAY</p>
+        </div>
+        <div id="right">
+        <p className="p">INDIA TODAY</p>
+        </div>
+        <div id="top"></div>
+        <div id="bottom"></div>
     </div>
+</div> 
+<div className="today">
+<div id="card1">
+      
+      <div class="front1">Today News</div>
+  </div>  
+<div class="vertical">
+    <ul>
+      <li>India strongly condemns Kabul terrorist attack</li>
+      <li>North Korea releases detained Russian fishing boat, says Russian embassy (Reuters)</li>
+      <li>Swimming: US set world record, claim women's 4x100m medley relay gold, reports Reuters</li>
+      <li>All the passengers of Mahalaxmi Express have been brought to Kolhapur via a special train, reports ANI</li>
+      <li>India strongly condemns Kabul terrorist attack</li>
+      <li>North Korea releases detained Russian fishing boat, says Russian embassy (Reuters)</li>
+      <li>Swimming: US set world record, claim women's 4x100m medley relay gold, reports Reuters</li>
+      <li>All the passengers of Mahalaxmi Express have been brought to Kolhapur via a special train, reports ANI</li>
+      <li>India strongly condemns Kabul terrorist attack</li>
+      <li>North Korea releases detained Russian fishing boat, says Russian embassy (Reuters)</li>
+      <li>Swimming: US set world record, claim women's 4x100m medley relay gold, reports Reuters</li>
+      <li>All the passengers of Mahalaxmi Express have been brought to Kolhapur via a special train, reports ANI</li>
+   
+    </ul>
+  </div>
+ 
+  </div>
+  </div>
+<div className="video">
+  <video controls>
+  <source src="movie.mp4" type="video/mp4"/>
+  <source src="movie.ogg" type="video/ogg"/>
+  Your browser does not support the video tag.
+</video>
+
+<video controls>
+  <source src="movie.mp4" type="video/mp4"/>
+  <source src="movie.ogg" type="video/ogg"/>
+  Your browser does not support the video tag.
+</video>
+</div>
+<div className="footer">
+<div id="cc">
+<div id="aa">
+<p id="p">
+HAPPY NEWS
+</p>
+</div>
+<div id="bb">
+<p id="p1">
+Choppers, Boats To Rescue Hundreds Stuck On Train Near Mumbai After Rain
+</p>
+</div>
+</div>
+
+<div className="bndiv">
+<p className="bn">FLASH NEWS</p>
+</div>
+<div className="marquee">
+
+<p className="marquee-text">A transform can be specified using the -webkit-transform property. It supports a list of functions, where each single function represents a transform operation to apply. You can chain together operations to apply multiple transforms at once to an object (e.g., if you want to both scale and rotate a box). The supported primitives are:
+
+scale, scaleX, scaleY – Scale an object around the transform origin. These functions take a number as an argument. The number can be negative (if you want to flip an object).
+rotate – Rotate an object around the transform origin. This function takes a CSS angle (e.g., degrees or radian units).
+translate, translateX, translateY – Translate an object. These functions take lengths or percentages as arguments. Percentages are relative to the border box of the object.
+skew, skewX, skewY – Skew an object. These functions take CSS angles.
+matrix – Specify a full affine transform matrix. This function takes six values. The last two values are the tx and ty, and they can be lengths or percentages.</p>
+
+</div>
+</div>
+</div>
   );
 }
 
